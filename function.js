578e1ff775ca872fbada6a58928c058546bab536
Peng Mallari <jennifergmallari@gmail.com>
2024-12-19 16:28:17 -0800

    additional buttons

--- function.js
+++ function.js
@@ -12,19 +12,4 @@ function showMenu() {
 function closeMenu() {
   document.querySelector('.nav-list-container').classList.remove("open");
 }
-
-document.getElementById("glucofit-page-btn").addEventListener("click", function (event) {
-  event.preventDefault(); // Prevent default navigation behavior
-
-  // Prompt the user for a password
-  const password = prompt("Enter the password to view the project:");
-
-  // Replace 'yourpassword' with the actual password
-  if (password === "yourpassword") {
-      // Redirect to the protected page
-      window.location.href = "../projects/glucofit.html";
-  } else if (password !== null) { // Null means the user canceled the prompt
-      alert("Incorrect password. Please try again.");
-  }
-});
   
